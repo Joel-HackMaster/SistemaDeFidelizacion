d86320f77fe131f851e5c4d43bd178701547d4ed
Joel-HackMaster <122944694+Joel-HackMaster@users.noreply.github.com>
2024-08-20 15:38:44 -0500

    first commit

---
+++ src/main/java/com/mycompany/sistemadefidelizacion/SistemaDeFidelizacion.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.sistemadefidelizacion;
+
+/**
+ *
+ * @author jwsb1
+ */
+public class SistemaDeFidelizacion {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
